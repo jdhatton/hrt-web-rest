ce0ea82915755dc6d69fc4a9e4fb33fc53be6562
jdhatton <jdhatton@gmail.com>
2015-07-21 19:36:57 -0500

    stubbed out invitation

---
+++ src/main/java/com/hrt/data/db/beans/Invitation.java
@@ -0,0 +1,9 @@
+package com.hrt.data.db.beans;
+
+public class Invitation {
+
+	public Invitation() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/hrt/web/resources/InvitationResource.java
@@ -0,0 +1,101 @@
+package com.hrt.web.resources;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.codahale.metrics.annotation.Timed;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.google.inject.Inject;
+import com.hrt.data.db.beans.Feedback;
+import com.hrt.data.db.beans.Invitation;
+import com.hrt.web.services.InvitationService;
+
+ 
+	@Path("/feedback")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public class InvitationResource extends JsonResource {
+
+		final static Logger logger = LoggerFactory.getLogger(InvitationResource.class);
+		private final AtomicLong counter;
+		private final InvitationService service;
+		
+		@Inject
+		public InvitationResource(InvitationService service) {
+			this.counter = new AtomicLong();
+			this.service = service;
+		}
+		
+		@POST
+		@Timed
+		@Path("/inviteStudent")
+		public Response inviteStudent( String json) {
+			
+			logger.debug(" ["+counter.incrementAndGet()+ "] InvitationResource::add()  :  JSON  =  " + json);
+
+			Invitation invitation = null;
+			 
+			try {
+				invitation = getMapper().readValue(json, Invitation.class);
+				if(invitation != null){
+					//service.processFeedback(feedback.getId(), feedback.getComment());
+				}
+			} catch (JsonParseException e) {
+				logger.error(">> JsonParseException :  " + e.getMessage());
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				logger.error(">> JsonMappingException :  " + e.getMessage());
+				e.printStackTrace();
+			} catch (IOException e) {
+				logger.error(">> IOException :  " + e.getMessage());
+				e.printStackTrace();
+			} catch(Exception e){
+				logger.error(">> Exception :  " + e.getMessage());
+				e.printStackTrace();
+			}
+			
+			return Response.status(Response.Status.OK).build();
+		}
+		
+		@POST
+		@Timed
+		@Path("/inviteParent")
+		public Response inviteParent( String json) {
+			
+			logger.debug(" ["+counter.incrementAndGet()+ "] FeedbackResource::add()  :  JSON  =  " + json);
+
+			Invitation invitation = null;
+			 
+			try {
+				invitation = getMapper().readValue(json, Invitation.class);
+				if(invitation != null){
+					//service.processFeedback(feedback.getId(), feedback.getComment());
+				}
+			} catch (JsonParseException e) {
+				logger.error(">> JsonParseException :  " + e.getMessage());
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				logger.error(">> JsonMappingException :  " + e.getMessage());
+				e.printStackTrace();
+			} catch (IOException e) {
+				logger.error(">> IOException :  " + e.getMessage());
+				e.printStackTrace();
+			} catch(Exception e){
+				logger.error(">> Exception :  " + e.getMessage());
+				e.printStackTrace();
+			}
+			
+			return Response.status(Response.Status.OK).build();
+		}
+	}
