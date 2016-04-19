e3a58fb305f17e7f2df19c224d2bdc2bb35a6bf4
jdhatton <jdhatton@gmail.com>
2016-04-18 19:41:12 -0500

    fixes for ios

--- src/main/java/com/hrt/web/HelloWorldApplication.java
+++ src/main/java/com/hrt/web/HelloWorldApplication.java
@@ -1,5 +1,11 @@
 package com.hrt.web;
 
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import io.federecio.dropwizard.swagger.SwaggerBundle;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -9,6 +15,7 @@ import com.hrt.data.db.dao.ClassroomStudentsDao;
 import com.hrt.data.db.dao.DistrictDao;
 import com.hrt.data.db.dao.FeedbackDao;
 import com.hrt.data.db.dao.StudentBehaviorDao;
+import com.hrt.data.db.dao.StudentDao;
 import com.hrt.data.db.dao.UserDao;
 import com.hrt.data.db.dao.ZipCodeDao;
 import com.hrt.data.db.dao.impl.ClassroomBehaviorDaoImpl;
@@ -17,6 +24,7 @@ import com.hrt.data.db.dao.impl.ClassroomStudentsDaoImpl;
 import com.hrt.data.db.dao.impl.DistrictDaoImpl;
 import com.hrt.data.db.dao.impl.FeedbackDaoImpl;
 import com.hrt.data.db.dao.impl.StudentBehaviorDaoImpl;
+import com.hrt.data.db.dao.impl.StudentDaoImpl;
 import com.hrt.data.db.dao.impl.UserDaoImpl;
 import com.hrt.data.db.dao.impl.ZipCodeDaoImpl;
 import com.hrt.web.dp.HelloWorldConfiguration;
@@ -25,20 +33,18 @@ import com.hrt.web.resources.HelloWorldResource;
 import com.hrt.web.resources.RemoteSyncResource;
 import com.hrt.web.resources.UserResource;
 import com.hrt.web.resources.ZipDistrictSchoolResource;
+import com.hrt.web.services.ClassroomService;
 import com.hrt.web.services.DistrictService;
 import com.hrt.web.services.FeedbackService;
+import com.hrt.web.services.StudentService;
 import com.hrt.web.services.UserService;
 import com.hrt.web.services.ZipCodeService;
+import com.hrt.web.services.impl.ClassroomServiceImpl;
 import com.hrt.web.services.impl.DistrictServiceImpl;
 import com.hrt.web.services.impl.FeedbackServiceImpl;
+import com.hrt.web.services.impl.StudentServiceImpl;
 import com.hrt.web.services.impl.UserServiceImpl;
 import com.hrt.web.services.impl.ZipCodeServiceImpl;
-
-import io.dropwizard.Application;
-import io.dropwizard.setup.Bootstrap;
-import io.dropwizard.setup.Environment;
-import io.federecio.dropwizard.swagger.SwaggerBundle;
-import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 
 
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
@@ -92,6 +98,9 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
 	            	bind(ZipCodeService.class).to(ZipCodeServiceImpl.class);
 	            	bind(ZipCodeDao.class).to(ZipCodeDaoImpl.class);
 	            	bind(UserService.class).to(UserServiceImpl.class);
+	            	bind(StudentService.class).to(StudentServiceImpl.class);
+	            	bind(ClassroomService.class).to(ClassroomServiceImpl.class);
+	            	
 	            	bind(UserDao.class).to(UserDaoImpl.class);
 	            	bind(FeedbackService.class).to(FeedbackServiceImpl.class);	
 	            	bind(FeedbackDao.class).to(FeedbackDaoImpl.class);	
@@ -99,6 +108,7 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
 	            	bind(ClassroomBehaviorDao.class).to(ClassroomBehaviorDaoImpl.class);
 	            	bind(ClassroomStudentsDao.class).to(ClassroomStudentsDaoImpl.class);
 	            	bind(StudentBehaviorDao.class).to(StudentBehaviorDaoImpl.class);
+	            	bind(StudentDao.class).to(StudentDaoImpl.class);
 	            	
 //	            	bind(NcesEdGovWrapper.class).to(NcesEdGovWrapperImpl.class);
 	            	//bind(MessagesConfiguration.class).toInstance(conf.getMessages()); // for ExampleResource, which does @Inject MessagesConfiguration
